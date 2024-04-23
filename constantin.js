3d4428dad58effbcdf647b5d974ed484014afe0a
isaac2c <isaac2.crosby@gmail.com>
2024-04-23 23:05:27 +0100

    create-file

---
+++ constantin.js
@@ -0,0 +1,32 @@
+// Files
+
+//Remember to escape forward slashes when passing fileLink as an argument.
+
+function newFile(fileName, fileType, fileLink) {
+    const fileTypes = [
+        {source: "\/uploaded-resources\/audio-transparent.png", alternative: "Audio file icon."},
+        {source: "\/uploaded-resources\/pdf-transparent.png", alternative: "PDF icon."},
+        {source: "\/uploaded-resources\/document-transparent.png", alternative: "Text document icon."},
+        {source: "\/uploaded-resources\/spreadsheet-transparent.png", alternative: "Spreadsheet icon."},
+        {source: "\/uploaded-resources\/image-transparent.png", alternative: "Image file icon."}
+    ];
+    const fileDirectory = document.querySelector("#file-directory");
+    const newLink = document.createElement("a");
+    newLink.setAttribute("href", fileLink);
+    newLink.setAttribute("download", fileName);
+    const newFigure = document.createElement("figure");
+    const newImage = document.createElement("img");
+    newImage.className += " doc-icon";
+    newImage.setAttribute("src", fileTypes[fileType].source);
+    newImage.setAttribute("alt", fileTypes[fileType].alternative);
+    const newCaption = document.createElement("figcaption");
+    newCaption.innerText = fileName;
+    newFigure.append(newImage, newCaption);
+    newLink.append(newFigure);
+    fileDirectory.append(newLink);
+}
+
+// Test file creation
+
+const testCreate = document.querySelector("#login-form");
+testCreate.setAttribute("onsubmit", "newFile(\"new-test-file.pdf\", 1, \"\/uploaded-resources\/downloadable-files\/example-pdf.pdf\")")
