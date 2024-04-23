5280da390823df5ac725c2bd3c73d972bf5575e0
isaac2c <isaac2.crosby@gmail.com>
2024-04-23 23:47:00 +0100

    Update constantin.js

--- constantin.js
+++ constantin.js
@@ -28,12 +28,11 @@ function newFile(fileName, fileType, fileLink) {
 
 // Test file creation
 
-
 testButton = document.createElement("button");
 testButton.innerText = "Test - Create New File";
 testFooter = document.querySelector("footer");
 testFooter.append(testButton);
-function testFile() {
+/*function testFile() {
     newFile("new-test-file.pdf", 1, "\/uploaded-resources\/downloadable-files\/example-pdf.pdf");
-}
+}*/
 testButton.onclick = testFile();
